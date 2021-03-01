ceae5ac03d92fe0ca39effdbddcf386d66614cf2
Nohemí <nohemi1180212345@gmail.com>
2021-03-01 00:56:40 -0700

    Modifiqué el codigo

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ namespace Arreglo_Bidimensional_
         static void Main(string[] args)
         {
        int[,] matriz1 = { {1,3,5}, {2,4,6}, {3,5,9}};
-       // [3,3]
-       //[5,4]
+       
 
        for (int i = 0; i < 3;i++){
            for (int j = 0; j < 3;j++){
