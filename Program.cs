27f9c684c48ad19a30d4ef6b923504120269a47e
Nohemí <nohemi1180212345@gmail.com>
2021-03-01 01:42:04 -0700

    Determinante 3x3

--- Program.cs
+++ Program.cs
@@ -9,13 +9,35 @@ namespace Arreglo_Bidimensional_
        int[,] matriz1 = { {1,3,5}, {2,4,6}, {3,5,9}};
        
 
-       for (int i = 0; i < 3;i++){
-           for (int j = 0; j < 3;j++){
-               Console.Write(matriz1[i,j] + " ");
+       for (int filas = 0; filas < 3;filas++){
+           for (int columnas = 0; columnas < 3;columnas++){
+               Console.Write(matriz1[filas,columnas] + " ");
            }
            Console.WriteLine();
        }
+            //Para calcular la determinante de una matriz se hacen multipliaciones 
+            //en diagonal de arriba hacia abajo y luego de abajo hacia arriba. 
 
+            //Multiplicacion de arriba hacia abajo en diagonal
+
+            int valor1 = matriz1[0,0] * matriz1[1,1] * matriz1[2,2];
+            int valor2 = matriz1[1,0] * matriz1[2,1] * matriz1[2,1];
+            int valor3 = matriz1[2,0] * matriz1[0,1] * matriz1[1,2];
+
+            //Multiplicacion de arriba hacia abajo en diagonal
+
+            int valor4 = matriz1[1,0] * matriz1[0,1] * matriz1[2,2];
+            int valor5 = matriz1[0,0] * matriz1[2,1] * matriz1[1,2];
+            int valor6 = matriz1[2,0] * matriz1[1,1] * matriz1[0,2];
+
+            int resultado1 = valor1 + valor2 + valor3;
+            int resultado2 = valor4 + valor5 + valor6;
+
+            int determinanteFinal = resultado1 - resultado2;
+
+            
+
+            Console.WriteLine(determinanteFinal);
         }
     }
 }
